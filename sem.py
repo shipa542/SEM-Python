1c96d3152fb17dbfe7f568e583e782ce11c82096
Shipa542 <shipa542@yandex.ru>
2022-09-06 18:48:25 +0300

    Задача №2

--- sem.py
+++ sem.py
@@ -2,25 +2,53 @@
 # Пример: - 6 -> да - 7 -> да - 1 -> нет  """
 
 
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Это не число!")
-    return number
+# def InputNumbers(inputText):
+#     is_OK = False
+#     while not is_OK:
+#         try:
+#             number = int(input(f"{inputText}"))
+#             is_OK = True
+#         except ValueError:
+#             print("Это не число!")
+#     return number
 
 
-def checkNumber(num):
-    if 6 <= num <= 7:
-        print("Да")
-    elif 0 < num < 6:
-        print("Нет")
-    else:
-        print("число вне пределов 7 дней")
+# def checkNumber(num):
+#     if 6 <= num <= 7:
+#         print("Да")
+#     elif 0 < num < 6:
+#         print("Нет")
+#     else:
+#         print("число вне пределов 7 дней")
 
 
-num = InputNumbers("Введите число: ")
-checkNumber(num)
+# num = InputNumbers("Введите число: ")
+# checkNumber(num)
+
+
+
+
+#  Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.  
+
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
