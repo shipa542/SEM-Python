1f9306d3e720e8f35149142470dc21c02facaef5
Shipa542 <shipa542@yandex.ru>
2022-09-06 19:05:06 +0300

    Задача №5

--- sem.py
+++ sem.py
@@ -99,14 +99,47 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
 
-n = int(input('Введите номер четверти: '))
-if n < 1 or n > 4:
-    print('Это не номер четверти!(от 1 до 4)')
-elif n == 1:
-    print('x > 0 and y > 0')
-elif n == 2:
-    print('x < 0 and y > 0')
-elif n == 3:
-    print('x < 0 and y < 0')
-elif n == 4:
-    print('x > 0 and y < 0')
+# n = int(input('Введите номер четверти: '))
+# if n < 1 or n > 4:
+#     print('Это не номер четверти!(от 1 до 4)')
+# elif n == 1:
+#     print('x > 0 and y > 0')
+# elif n == 2:
+#     print('x < 0 and y > 0')
+# elif n == 3:
+#     print('x < 0 and y < 0')
+# elif n == 4:
+#     print('x > 0 and y < 0')
+
+
+# Напишите программу, которая принимает на вход координаты двух точек 
+# и находит расстояние между ними в 2D пространстве. 
+# Пример: - A (3,6); B (2,1) -> 5,09 - A (7,-5); B (1,-1) -> 7,21 
+
+
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Ты ошибся. Вводить надо целые числа!")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
