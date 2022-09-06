2a1e294f71484f37509db3a210501954a0e3b1e5
Shipa542 <shipa542@yandex.ru>
2022-09-06 18:52:13 +0300

    Задача №3

--- sem.py
+++ sem.py
@@ -31,24 +31,64 @@
 #  Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.  
 
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
+# def inputNumbers(x):
+#     xyz = ["X", "Y", "Z"]
+#     a = []
+#     for i in range(x):
+#         a.append(input(f"Введите значение {xyz[i]}: "))
+#     return a
+
+
+# def checkPredicate(x):
+#     left = not (x[0] or x[1] or x[2])
+#     right = not x[0] and not x[1] and not x[2]
+#     result = left == right
+#     return result
+
+
+# statement = inputNumbers(3)
+
+# if checkPredicate(statement) == True:
+#     print(f"Утверждение истинно")
+# else:
+#     print(f"Утверждение ложно")
+
+
+
+
+
+# Напишите программу, которая принимает на вход координаты точки (X и Y), 
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится). 
+# Пример: - x=34; y=-30 -> 4 - x=2; y=4-> 1 - x=-34; y=-30 -> 3  
+
+
+def inputKoord(x):
+    a = [0] * x
     for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Ты ошибся. Вводить надо числа!")
     return a
 
 
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
 
 
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
